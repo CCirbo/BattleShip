32b8f17a19bd12ce293c8d4805d0a834bc65493f
Candice Cirbo <thecirbos@icloud.com>
2024-07-19 20:40:25 -0600

    Cleans up minor formatting

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,23 +9,23 @@ RSpec.configure do |config|
     @cruiser = Ship.new("Cruiser", 3)
   end
 
-  describe "#initialize" do
-    it "exists" do
+  describe '#initialize' do
+    it 'exists' do
       expect(@cruiser).to be_a(Ship)
     end
 
-    it "has attributes" do
+    it 'has attributes' do
       expect(@cruiser.name).to eq("Cruiser")
       expect(@cruiser.length).to eq(3)
     end
 
-    it "has a health equal to length" do
+    it 'has a health equal to length' do
       expect(@cruiser.health).to eq(3)
     end
   end
 
-  describe "#sunk" do
-    it "can show ship not sunk is false by default" do
+  describe '#sunk' do
+    it 'can show ship not sunk is false by default' do
       expect(@cruiser.sunk?).to eq(false)
     end
 
