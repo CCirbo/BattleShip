b3ec80c817db8ac3231336a5116d613febd846a3
Candice Cirbo <thecirbos@icloud.com>
2024-07-19 20:39:54 -0600

    Cleans up minor formatting

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -12,46 +12,46 @@ RSpec.describe Cell do
     @cruiser = Ship.new("Cruiser", 3)
   end
 
-  describe "#initalize()" do
-    it "can exist" do
+  describe '#initalize()' do
+    it 'can exist' do
       expect(@cell).to be_a(Cell)
     end
 
-    it "has a coordinate" do
+    it 'has a coordinate' do
       expect(@cell.coordinate).to eq("B4")
     end
   end
 
-  describe "#cell status" do
+  describe '#cell status' do
     it "checks if a cell can hold a ship" do
       expect(@cell.ship).to eq(nil)
     end 
 
-    it "can tell if the cell is empty" do
+    it 'can tell if the cell is empty' do
       expect(@cell.empty?).to eq(true)
     end
   end
 
-  describe "#ships" do
-    it "can place a ship on an empty cell" do
+  describe '#ships' do
+    it 'can place a ship on an empty cell' do
       @cell.place_ship(@cruiser)
       expect(@cell.ship).to eq(@cruiser)
     end
 
-    it "can tell if cell is not empty" do
+    it 'can tell if cell is not empty' do
       @cell.place_ship(@cruiser)
       expect(@cell.ship).to eq(@cruiser)
       expect(@cell.empty?).to eq(false)
     end
   end
   
-  describe "#fired_upon?" do
+  describe '#fired_upon?' do
     it 'has fired_upon? assigned false by default' do
       expect(@cell.fired_upon?).to eq(false)
     end
   end
 
-  describe "#fire_upon" do
+  describe '#fire_upon' do
     it 'will update ship health if ship has been fired upon' do
       @cell.place_ship(@cruiser)
       expect(@cell.fired_upon).to eq(false)
@@ -62,7 +62,7 @@ RSpec.describe Cell do
     end
   end
 
-  describe "#render board()" do
+  describe '#render board()' do
     it 'shows the string "." as default' do
       expect(@cell_1.render).to eq(".")
     end
